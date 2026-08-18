67949c5dbc8f0e6da4eb0aa90dda5a59048c1e2f
divine-mercyx <divineobinali9@gmail.com>
2026-08-18 14:05:48 +0100

    commit

---
+++ daytwo/java/PalindromePrime.java
@@ -0,0 +1,18 @@
+public class PalindromePrime{
+	public static void main(String [] args){
+	
+	int number = 12321;
+	
+	for(int count = 1 ; count <= number ; count++)
+		for(int index = 1 ; index <= count ; index++)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
---
+++ daytwo/python/Skip.py
@@ -0,0 +1,12 @@
+numbers = 100
+
+for number in range(1,101):
+	if number % 3 == 0:
+		print("skip")
+	else:
+		print(number)
+
+
+
+
+
